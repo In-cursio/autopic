7ad608fc697fce46da7616ca610790b765790c14
sathanxavier1998 <71935696+sathanxavier1998@users.noreply.github.com>
2023-10-04 18:15:12 +0530

    Update __init__.py

--- autopicx/__init__.py
+++ autopicx/__init__.py
@@ -7,10 +7,10 @@ from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-API_ID = int(environ.get("API_ID"))
-API_HASH = environ.get("API_HASH")
-SESSION = environ.get("SESSION")
-TIME = int(environ.get("TIME", "60"))
+API_ID = int(environ.get("API_ID", "6620972"))
+API_HASH = environ.get("API_HASH", "3f6835286b03e000ab6d71b37cc35b92")
+SESSION = environ.get("SESSION", "BQCxw1pNin5BLpgc22AQpWdQlINVqBN93Oenoe7w8i00GUWd7_PVjvuo5TaHlFoA5gICVHX9UfMKEfNkKeaDIjVnOqyqOzkv_TWzBLcivQMWR8-jTb9bM5yFy5ahD1OtaXsBsKwIUN-OTJulsUIJu1-tmbtge2-voAIjolbky_q8mvS2kYOJSP_9luUU0GUn52wX9h8BNm4WxPL-iVlq7j1YPUu5sQCZT0MhnxgQOp2HsLXeJiWyGfn7lszYhZSktoB3rpFCGZufHuoGFYbW8pxavyCx_lEK4R463Di8HhFqPtZ628CfDwxRYgK04miv8liF5_ShpOHkp6QgAk7X_lTBaANPBgA")
+TIME = int(environ.get("TIME", "600"))
 CHANNEL_ID = int(environ.get("CHANNEL_ID", "-1001836315219"))
 
 
